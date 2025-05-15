d5fe67bf569f2b7b4bed255c7caea0f3dd288e89
TheNawaf5 <Jacob5355355@gmail.com>
2025-05-15 16:03:49 +0300

    The End

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene"); // Replace with your actual game scene name
+        SceneManager.LoadSceneAsync("SampleScene"); // Replace with your actual game scene name
     }
 
     public void QuitGame()
