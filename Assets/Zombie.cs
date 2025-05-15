62709d861d77164d0937d75bd564a256d2b75169
TheNawaf5 <Jacob5355355@gmail.com>
2025-05-15 08:39:21 +0300

    a

---
+++ Assets/Zombie.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField] private int HP = 100;
+    private Animator animator;
+    private NavMeshAgent navAgent;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+
+        if (HP <= 0)
+        {
+            animator.SetTrigger("DIE");
+            Destroy(gameObject);
+        }
+        else
+        {
+            animator.SetTrigger("DAMAGE");
+        }
+    }
+
+    private void Update()
+    {
+        if (navAgent.velocity.magnitude < 0.1f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+    }
+}
